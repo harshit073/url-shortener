90b8d335f90f5a69835dd175872576659f566095
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-07-21 23:31:28 +0530

    commenting done

--- src/validator/validation.js
+++ src/validator/validation.js
@@ -1,4 +1,5 @@
 
+//validation for empty request body
 const isValidRequest = function(data){
     if(Object.keys(data).length == 0){
       return false
@@ -6,12 +7,14 @@ const isValidRequest = function(data){
     return true
   }
 
+  //String validation
   const isValidString = function (value) {
     if (typeof value == undefined || value == null) return false;
     if (typeof value == "string" && value.trim().length == 0) return false;
     else if (typeof value == "string") return true;
   }
 
+  //validation for format of long URL
   const isValidURL = function(url){
     return (/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/).test(url)
 
